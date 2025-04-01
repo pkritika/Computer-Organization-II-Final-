592e3ac71b4f8e57d548b91bfa00c446b30f6edc
pkritika <kritika.pant@bison.howard.edu>
2025-03-31 20:40:40 -0400

    Created calculations for addition, multiplication and division

--- floatingBench.py
+++ floatingBench.py
@@ -1,5 +1,22 @@
 import time
 
 #Putting constant values 
-dummy_value = 8.4
+dummy_value = 6.2
 res = 1.2
+
+start = time.time()
+
+# 10^10 additions which is for double precision floating point constants
+for i in range(1, 100001):
+    for j in range(1, 100001):
+        res = dummy_value + dummy_value
+
+# 5 × 10^9 multiplications which is double precision floating point constants
+for i in range(1, 100001):
+    for j in range(1, 50001):
+        res = dummy_value * dummy_value
+
+# 2 × 10^9 divisions which is double precision floating point constants
+for i in range(1, 100001):
+    for j in range(1, 20001):
+        res = dummy_value / dummy_value
