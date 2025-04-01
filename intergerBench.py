10fc6f72bc00bfb394c64a411f4d51b3d9fbc6c6
pkritika <kritika.pant@bison.howard.edu>
2025-03-31 21:21:29 -0400

    Calculations for addition, multiplication and division

--- intergerBench.py
+++ intergerBench.py
@@ -4,3 +4,17 @@ import time
 dummy_value = 6
 res = 3
 
+# 10^10 additions which is for double precision floating point constants
+for i in range(1, 100001):
+    for j in range(1, 100001):
+        res = dummy_value + dummy_value
+
+# 5 × 10^9 multiplications which is double precision floating point constants
+for i in range(1, 100001):
+    for j in range(1, 50001):
+        res = dummy_value * dummy_value
+
+# 2 × 10^9 divisions which is double precision floating point constants
+for i in range(1, 100001):
+    for j in range(1, 20001):
+        res = dummy_value / dummy_value
