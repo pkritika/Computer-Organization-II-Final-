51aa21025d7011d0196abc047baa0a13501199f3
pkritika <kritika.pant@bison.howard.edu>
2025-03-31 21:24:36 -0400

    Calculated the total time by subtracting end and start time

--- intergerBench.py
+++ intergerBench.py
@@ -3,7 +3,7 @@ import time
 #Putting constant values 
 dummy_value = 6
 res = 3
-
+start = time.time()
 # 10^10 additions which is for double precision floating point constants
 for i in range(1, 100001):
     for j in range(1, 100001):
@@ -18,3 +18,10 @@ for i in range(1, 100001):
 for i in range(1, 100001):
     for j in range(1, 20001):
         res = dummy_value / dummy_value
+
+# timing to the end
+end = time.time()
+total_time = end - start
+
+
+
