f6d881238d48123af958a31e81f3df3fb8323477
pkritika <kritika.pant@bison.howard.edu>
2025-04-02 16:23:26 -0400

    Printing the benchmarking data in different colors

--- memoryBench.cpp
+++ memoryBench.cpp
@@ -29,5 +29,11 @@ int main() {
 auto end = chrono::high_resolution_clock::now();
 auto diff = chrono::duration_cast<std::chrono::duration<double> >(end - start);
 totalTime = diff.count();
+cout << ANSI_COLOR_YELLOW << "Benchmark 3:" << endl << ANSI_COLOR_RESET;
+cout << ANSI_COLOR_GREEN << "Memory benchmark" << endl << ANSI_COLOR_RESET;
+cout << ANSI_COLOR_RED << "Which includes reading from, and writing to array, 4 bytes each time' is: " << totalTime << " seconds" << ANSI_COLOR_RESET << endl;
 
+delete[] myArr;
+
+return 0;
 }
