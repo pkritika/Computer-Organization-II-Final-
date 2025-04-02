5c6025b1b60de35ab2c664ea21b2fa21cbaa424d
pkritika <kritika.pant@bison.howard.edu>
2025-04-02 15:56:15 -0400

    10^10 additions (of double precision floating point constants)

--- floatingBench.cpp
+++ floatingBench.cpp
@@ -12,4 +12,12 @@ int main() {
     double dummyValue = 6.4;
     double res = 2.2;
 
+    auto start = chrono::high_resolution_clock::now();
+
+    for(register int i = 1; i <= 100000; i++){
+        for (register int j = 1; j <= 100000; j++){
+            res = dummyValue + dummyValue;
+        }
+
+    }
 }
