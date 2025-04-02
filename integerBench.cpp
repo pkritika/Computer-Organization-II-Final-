031b6f341b70c030e6f403dbf4126b06b4aea9b4
pkritika <kritika.pant@bison.howard.edu>
2025-04-02 15:03:18 -0400

    10^10 additions (of integer constants)

--- integerBench.cpp
+++ integerBench.cpp
@@ -13,3 +13,12 @@ int main() {
     double totalTime;
     int32_t dummyValue = 5;
     int32_t res = 0;
+
+    auto start = chrono::high_resolution_clock::now();
+
+    for(register int i = 1; i <= 100000; i++){
+        for (register int j = 1; j <= 100000; j++){
+            res = dummyValue + dummyValue;
+        }
+
+    }    
