3c62ab0ebf0ff7a149bc3f11c6cbf6665d68b27e
pkritika <kritika.pant@bison.howard.edu>
2025-04-02 16:20:44 -0400

    Writing to 5 × 10^9 different array elements, 4 bytes each time

--- memoryBench.cpp
+++ memoryBench.cpp
@@ -9,3 +9,16 @@ using namespace std;
 #define ANSI_COLOR_GREEN   "\x1b[32m"
 #define ANSI_COLOR_YELLOW  "\x1b[33m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
+
+
+int main() {
+    int32_t* myArr = new int32_t[5000000000];
+    int32_t dummyReadValue = 0;
+    double totalTime;
+    
+    auto start = chrono::high_resolution_clock::now();
+
+    for(long long int i = 1; i <= 5000000000; i++){
+            myArr[i-1] = i;
+    }
+}
