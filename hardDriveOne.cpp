22536d16222b03424e26aa5a6f7b7fa8ab5fc233
pkritika <kritika.pant@bison.howard.edu>
2025-04-02 16:46:05 -0400

    Reading a whole file of 10^9 bytes, 100 bytes each time

--- hardDriveOne.cpp
+++ hardDriveOne.cpp
@@ -11,3 +11,22 @@ using namespace std;
 #define ANSI_COLOR_GREEN   "\x1b[32m"
 #define ANSI_COLOR_YELLOW  "\x1b[33m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
+
+
+int main() {
+
+    ofstream outputFileHandler("dummyOneBillionBytes.txt");
+    long long int totalFileSize = 1000000000;
+    int oneTimeSize = 100;
+    long long int bytesWrittenSoFar = 0;
+    double totalTime = 0;
+
+    auto start = chrono::high_resolution_clock::now();
+
+    char outputBuffer[oneTimeSize];
+    memset(outputBuffer, 'A', oneTimeSize);
+
+    while(bytesWrittenSoFar < totalFileSize){
+        outputFileHandler << string(outputBuffer);
+        bytesWrittenSoFar = bytesWrittenSoFar + oneTimeSize;
+    }
